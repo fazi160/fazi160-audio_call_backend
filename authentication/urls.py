99c412c69a1f7c0bd26e2afa46c00ffe8df50da6
fazil <fazifazil160160@gmail.com>
2025-06-22 01:58:03 +0530

    bug fix

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from . import views
+from .views import *
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login'),
-    path('logout/', views.logout_user, name='logout'),
-    path('webauthn/register/begin/', views.webauthn_register_begin, name='webauthn_register_begin'),
-    path('webauthn/register/complete/', views.webauthn_register_complete, name='webauthn_register_complete'),
-    path('webauthn/authenticate/begin/', views.webauthn_authenticate_begin, name='webauthn_authenticate_begin'),
-    path('webauthn/authenticate/complete/', views.webauthn_authenticate_complete, name='webauthn_authenticate_complete'),
-    path('profile/', views.user_profile, name='user_profile'),
+    path('register/', register_user, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('webauthn/register/begin/', webauthn_register_begin, name='webauthn_register_begin'),
+    path('webauthn/register/complete/', webauthn_register_complete, name='webauthn_register_complete'),
+    path('webauthn/authenticate/begin/', webauthn_authenticate_begin, name='webauthn_authenticate_begin'),
+    path('webauthn/authenticate/complete/', webauthn_authenticate_complete, name='webauthn_authenticate_complete'),
+    path('profile/', user_profile, name='user_profile'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
